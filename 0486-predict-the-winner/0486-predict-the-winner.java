3f19295481b4204833110284f903d0d846eb0c89
Sameer Jha <sameerjha906@gmail.com>
2026-08-02 01:13:26 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.02%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,16 +1,23 @@
 class Solution {
+    Integer[][] dp = new Integer[23][23];
+
     int solve(int nums [], int left, int right){
         if(left>right){
             return 0;
         }
+        if (dp[left][right] != null) {
+            return dp[left][right];
+        }
         if(left == right){
-            return nums[left];
+            return dp[left][right] = nums[left];
         }
         int pickL = nums[left] - solve(nums,left+1,right);
         int pickR = nums[right] - solve(nums,left,right-1);
-        return Math.max(pickL,pickR);
+        return dp[left][right] = Math.max(pickL,pickR);
     }
     public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        dp = new Integer[n][n];
         return solve(nums,0,nums.length-1) >=0;
     }
 }
