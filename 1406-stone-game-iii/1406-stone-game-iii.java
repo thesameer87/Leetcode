38558d09e6726665f5297d57d555758f431ce446
Sameer Jha <sameerjha906@gmail.com>
2026-08-04 02:23:37 +0530

    Time: 6 ms (91.96%), Space: 85.4 MB (73.12%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -4,10 +4,10 @@ class Solution {
         int dp[] = new int[n + 1];
         for (int i = n - 1; i >= 0; i--) {
             dp[i] = stoneValue[i] - dp[i + 1];
-            if (i + 1 < n) {
+            if (i + 2 <= n) {
                 dp[i] = Math.max(dp[i], stoneValue[i] + stoneValue[i + 1] - dp[i + 2]);
             }
-            if (i + 2 < n) {
+            if (i + 3 <= n) {
                 dp[i] = Math.max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3]);
             }
         }
