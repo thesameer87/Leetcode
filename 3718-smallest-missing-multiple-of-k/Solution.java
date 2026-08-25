c3a4e581e69b3cb6f9df72d3d0e703dd76ac1a3e
Sameer Jha <sameerjha906@gmail.com>
2026-08-25 18:07:41 +0530

    CodeSync: Add solution for Smallest Missing Multiple of K

---
+++ 3718-smallest-missing-multiple-of-k/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i =0 ; i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        for(int i = 1; ;i++){
+            int num = k * i;
+            if(!set.contains(num)){
+                return num;
+            }
+        }
+        
+
+    }
+}
